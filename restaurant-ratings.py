6329f1f11196d3c85444f8394184a065e113f465
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 17:50:24 -0800

    new restaurant & rating

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,8 +1,12 @@
 #Pair programming emilymlam & tjhakseth
 def restaurant_rating (filename):
     file_to_open = open(filename)
+
+    new_restaurant_name, new_restaurant_rating = new_restaurant()
     
     restaurant_list = {}
+
+    restaurant_list[new_restaurant_name] = new_restaurant_rating
 
     for line in file_to_open:
         restaurant_name, rating = line.rstrip().split(":")
@@ -11,5 +15,11 @@ def restaurant_rating (filename):
     sorted_keys = sorted(restaurant_list.keys())
     for key in sorted_keys:
         print key + " : " + restaurant_list[key]
+
+def new_restaurant():
+
+    restaurant_name_input = raw_input("Enter restaurant name: ")
+    restaurant_rating_input = raw_input("Enter restaurant score: ")
+    return restaurant_name_input, restaurant_rating_input
 
 restaurant_rating("scores.txt")
