f709f38e7ab448f5c30695d9e009b122f3394e83
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 17:32:40 -0800

    working sorted ratings

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -6,23 +6,10 @@ def restaurant_rating (filename):
 
     for line in file_to_open:
         restaurant_name, rating = line.rstrip().split(":")
-        restaurant_list [restaurant_name] = rating
-
+        restaurant_list[restaurant_name] = rating
 
     sorted_keys = sorted(restaurant_list.keys())
-    print sorted_keys 
-
     for key in sorted_keys:
-
-
-
-
-
-
-        #key = sorted(restaurant_file[0])
-        #restaurant_list = (key: 0)
-
-        # for rating in restaurant_list:
-        #     restaurant_list[rating] = sorted(restaurant_list.get(rating, 0))
+        print key + " : " + restaurant_list[key]
 
 restaurant_rating("scores.txt")
